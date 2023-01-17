7fb312b99a2e23964d9849060333342439d090b8
Crimean-Frost <104132812+Crimean-Frost@users.noreply.github.com>
2023-01-17 17:38:18 +0300

    Add files via upload

---
+++ Yakub/js/app.js
@@ -0,0 +1,19 @@
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction() {
+    document.getElementById("myDropdwn").classList.toggle("show");
+  }
+  
+  // Закройте выпадающее меню, если пользователь щелкает за его пределами
+  window.onclick = function(event) {
+    if (!event.target.matches('.drpbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdwn-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
